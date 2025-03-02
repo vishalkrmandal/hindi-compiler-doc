f8e00ac7cebede692873d542d9ec4d4f97583c6f
Vishalkrmandal <vishalparikshitmandal@gmail.com>
2025-03-03 03:17:29 +0530

    first commit

--- vite.config.ts
+++ vite.config.ts
@@ -7,9 +7,11 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: "/hindi-compiler-doc",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
 })
+ 
